4d9b1b87e57039eb6d809e9a3ab58533ec3aac69
Patrick Lavin <plavin997@gmail.com>
2024-04-19 10:02:08 -0600

    add test programs for ariel mpi

---
+++ src/sst/elements/ariel/tests/testMPI/test-mpi.py
@@ -0,0 +1,122 @@
+import sst
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog=f'sst [sst-args] test-mpi.py --',
+    description='Used for testing Ariel\'s MPI features')
+
+parser.add_argument('program', help='Which program to run. Either "hello" or "reduce".')
+parser.add_argument('-r', dest='ranks', default=1, help='How many ranks of the traced program to run.')
+parser.add_argument('-a', dest='tracerank', default=0, help='Which of the MPI ranks will be traced.')
+parser.add_argument('-t', dest='threads', default=1, help='The number of OpenMP threads to use per rank.')
+parser.add_argument('-s', dest='size', default=1024, help='The input value for the "reduce" program')
+
+args = parser.parse_args()
+
+ncores    = int(args.threads)
+mpiranks  = int(args.ranks)
+tracerank = int(args.tracerank)
+size      = int(args.size)
+
+if args.program not in ['hello', 'reduce']:
+    print('Error: supported values for `program` are "hello" and "reduce".')
+    sys.exit(1)
+
+
+print(f'Running with {mpiranks} ranks and {ncores} threads per rank. Tracing rank {tracerank}. Size {size}')
+sys.exit(0)
+
+os.environ['OMP_NUM_THREADS'] = str(ncores)
+
+#########################################################################
+## Define SST core options
+#########################################################################
+# If this demo gets to 100ms, something has gone very wrong!
+sst.setProgramOption("stop-at", "200ms")
+
+#########################################################################
+## Declare components
+#########################################################################
+core    = sst.Component("core", "ariel.ariel")
+cache   = [sst.Component("cache_"+str(i), "memHierarchy.Cache") for i in range(ncores)]
+memctrl = sst.Component("memory", "memHierarchy.MemController")
+bus     = sst.Component("bus", "memHierarchy.Bus")
+
+#########################################################################
+## Set component parameters and fill subcomponent slots
+#########################################################################
+# 2.4GHz cores. One for each omp thread
+core.addParams({
+    "clock"        : "2.4GHz",
+    "verbose"      : 1,
+    "executable"   : f"./{args.program}",
+    "arielmode"    : 0, # Disable tracing at start
+    "corecount"    : ncores,
+    "mpilauncher"  : "./mpilauncher-onenode",
+    "mpiranks"     : mpiranks,
+    "mpitracerank" : tracerank,
+})
+
+if args.program == "reduce":
+    core.addParams({
+        "appargcount" : 1,
+        "apparg0" : size,
+    })
+
+# Cache: L1, 2.4GHz, 2KB, 4-way set associative, 64B lines, LRU replacement, MESI coherence
+for i in range(ncores):
+    cache[i].addParams({
+        "L1" : 1,
+        "cache_frequency" : "2.4GHz",
+        "access_latency_cycles" : 2,
+        "cache_size" : "2KiB",
+        "associativity" : 4,
+        "replacement_policy" : "lru",
+        "coherence_policy" : "MESI",
+        "cache_line_size" : 64,
+    })
+
+# Memory: 50ns access, 1GB
+memctrl.addParams({
+    "clock" : "1GHz",
+    "backing" : "none", # We're not using real memory values, just addresses
+    "addr_range_end" : 1024*1024*1024-1,
+})
+memory = memctrl.setSubComponent("backend", "memHierarchy.simpleMem")
+memory.addParams({
+    "mem_size" : "1GiB",
+    "access_time" : "50ns",
+})
+
+bus.addParams({
+    "bus_frequency": "2.0GHz",
+})
+
+#########################################################################
+## Declare links
+#########################################################################
+core_cache = [sst.Link("core_to_cache_"+str(i)) for i in range(ncores)]
+cache_bus  = [sst.Link("cache_" + str(i) + "_to_bus") for i in range(ncores)]
+bus_mem    = sst.Link("bus_to_memory")
+
+
+#########################################################################
+## Connect components with the links
+#########################################################################
+[core_cache[i].connect( (core, "cache_link_"+str(i), "100ps"), (cache[i], "high_network_0", "100ps") ) for i in range(ncores)]
+[cache_bus[i].connect( (cache[i], "low_network_0", "100ps"), (bus, "high_network_"+str(i), "100ps") ) for i in range(ncores)]
+bus_mem.connect( (bus, "low_network_0", "100ps"), (memctrl, "direct_link", "100ps") )
+
+sst.setStatisticOutput("sst.statoutputtxt")
+
+# Send the statistics to a fiel called 'stats.csv'
+sst.setStatisticOutputOptions( { "filepath"  : "stats.csv" })
+
+# Print statistics of level 5 and below (0-5)
+sst.setStatisticLoadLevel(5)
+
+# Enable statistics for all the component
+sst.enableAllStatisticsForAllComponents()
+################################ The End ################################
